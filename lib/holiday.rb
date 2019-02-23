51c314f17c7a34f12d11fa5560d5685f853c1e26
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-23 22:05:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,6 +48,10 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  
+  holiday_hash.map do |season, holiday|
+    binding.pry 
+  end 
 
 end
 
