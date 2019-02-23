c8b46ddcf082c691ecede009d06e9a67a3fd38b5
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-23 21:26:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -12,7 +12,6 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
 #3
 def add_supply_to_memorial_day(holiday_hash, supply)
   holiday_supplies[:spring][:memorial_day]<< supply 
-  holiday_supplies
 end
 
 #4
